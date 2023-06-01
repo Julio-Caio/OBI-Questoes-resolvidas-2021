d45820872364a0490975d8152d74725620d752e2
Júlio Caio <101609625+Julio-Caio@users.noreply.github.com>
2023-06-01 23:45:23 +0000

    Acelerador de partículas da Fase 1 de 2020

---
+++ acelerador.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+circ = 8
+
+dist_1 = 6
+dist_2 = 7
+dist_3 = 8
+
+if n % circ == dist_1:
+  print(1)
+elif n % circ == dist_2:
+    print(2)
+
+elif n % circ == 0:
+     print(3)
